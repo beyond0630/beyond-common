cb2e6a395940a5b61657649bfe5434afa7b7bbff
beyond <bin_beyond@outlook.com>
2022-12-06 09:18:18 +0800

    chore: ObjectMapperr

--- src/main/java/com/beyond/json/StandardObjectMapper.java
+++ src/main/java/com/beyond/json/StandardObjectMapper.java
@@ -5,13 +5,24 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public final class StandardObjectMapper extends ObjectMapper {
 
-    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssXXX";
+    public static final String DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
 
     public StandardObjectMapper() {
         super();
@@ -19,10 +30,20 @@ public final class StandardObjectMapper extends ObjectMapper {
         simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
         simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
         this.registerModule(simpleModule);
-        this.registerModule(new Jdk8Module());
+
+        final JavaTimeModule timeModule = new JavaTimeModule();
+        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DEFAULT_DATETIME_FORMAT);
+        timeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter));
+        timeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormatter));
+        timeModule.addSerializer(LocalDate.class, new LocalDateSerializer(dateTimeFormatter));
+        timeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(dateTimeFormatter));
+        timeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(dateTimeFormatter));
+        timeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(dateTimeFormatter));
+        this.registerModule(timeModule);
+
         this.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .setDateFormat(new SimpleDateFormat(DEFAULT_DATE_FORMAT));
+            .setDateFormat(new SimpleDateFormat(DEFAULT_DATETIME_FORMAT));
     }
 
     public static StandardObjectMapper getInstance() {
