ffdc6512590bf16f3ecd2cb176f4460850ecfdb5
beyond <bin_beyond@outlook.com>
2023-02-02 13:49:45 +0800

    feat: 添加雪花 ID

---
+++ src/main/java/com/beyond/id/IdFactory.java
@@ -0,0 +1,15 @@
+package com.beyond.id;
+
+/**
+ * @author beyond
+ * @since 2022/12/2
+ */
+public interface IdFactory {
+
+    /**
+     * 生成 Id
+     *
+     * @return long
+     */
+    long generate();
+}
